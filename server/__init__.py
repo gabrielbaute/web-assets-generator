2db7fb95d03e2b38ced7dd5a8bbc9702c1741120
gabrielbaute <gabrielbaute@gmail.com>
2025-03-31 19:50:16 -0400

    Logger inicializado en funcion de construccion

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from config import current_config
+from server_logging import setup_logging
 from server.routes import register_blueprints
 from server.extensions import register_extensions, register_context_processors
 from server.handlers import register_error_handlers
@@ -18,6 +19,7 @@ def create_app(config_class=None):
     # 2. Carga de configuración (prioridad: parámetro > entorno > current_config)
     config_obj = config_class or os.getenv('FLASK_CONFIG_CLASS') or current_config
     app.config.from_object(config_obj)
+    setup_logging(app)
     
     # 3. Validación de configuraciones
     config_obj.validate()
