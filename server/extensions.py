a2b1176a80bd309d0f44e562d910e450e2f31b73
gabrielbaute <gabrielbaute@gmail.com>
2025-03-30 21:28:22 -0400

    Archivo de configuración de extensiones futuras

---
+++ server/extensions.py
@@ -0,0 +1,14 @@
+
+def register_extensions(app):
+    """Registra extensiones como Flask-SQLAlchemy, Flask-Login, etc."""
+    pass  # Añade aquí tus extensiones si las necesitas
+
+def register_context_processors(app):
+    """Inyecta variables globales a los templates."""
+    
+    @app.context_processor
+    def inject_globals():
+        return {
+            'app_name': app.config['APP_NAME'],
+            'debug_mode': app.config['DEBUG']
+        }
